7f7abb6f3187c48b9947db5bafbcd502b4961c9c
cmghoglund <cmghoglund@gmail.com>
2023-07-09 17:03:03 +0900

    Encapsulated all code within functions to further modularize the script. Used transactions for more efficient inserts. Limited the output to the first 10 rows to avoid cluttering the terminal window.

--- main.py
+++ main.py
@@ -3,73 +3,80 @@
 import sqlite3
 import random
 
-# Function to generate a random name
 def generate_random_name():
     first_names = ['Yuki', 'Hiroshi', 'Sakura', 'Naoko', 'Kenji', 'Ayumi', 'Takeshi', 'Yui', 'Daiki', 'Riko']
     last_names = ['Sato', 'Suzuki', 'Takahashi', 'Tanaka', 'Watanabe', 'Ito', 'Nakamura', 'Kobayashi', 'Yamamoto', 'Kato']
     return random.choice(first_names) + " " + random.choice(last_names)
 
-# Function to generate a random email
 def generate_random_email(name):
     domains = ['example.co.jp', 'mail.jp', 'webmail.jp']
     return name.lower().replace(' ', '.') + '@' + random.choice(domains)
 
-# Function to generate a random city
 def generate_random_city():
     cities = ['Tokyo', 'Osaka', 'Nagoya', 'Sapporo', 'Fukuoka', 'Kobe', 'Yokohama', 'Kyoto', 'Hiroshima', 'Sendai']
     return random.choice(cities)
 
-# Connect to the database (or create one if it doesn't exist)
-connection = sqlite3.connect('test_database.db')
+def create_table(cursor):
+    # Delete the table if it exists
+    cursor.execute("DROP TABLE IF EXISTS users;")
+    # Create a table
+    cursor.execute("""
+        CREATE TABLE users (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            age INTEGER,
+            email TEXT,
+            city TEXT,
+            is_employee BOOLEAN
+            );"""
+    )
 
-# Create a cursor object
-cursor = connection.cursor()
+def insert_random_users(cursor, num_users):
+    user_data = []
 
-# Delete the table if it exists
-cursor.execute("DROP TABLE IF EXISTS users;")
+    for _ in range(num_users):
+        name = generate_random_name()
+        age = random.randint(15, 65)
+        email = generate_random_email(name)
+        city = generate_random_city()
+        is_employee = random.choice([True, False])
 
-# Create a table
-cursor.execute("""
-    CREATE TABLE users (
-        id INTEGER PRIMARY KEY,
-        name TEXT,
-        age INTEGER,
-        email TEXT,
-        city TEXT,
-        is_employee BOOLEAN
-        );"""
-)
+        user_data.append((name, age, email, city, is_employee))
 
-# Generate data for several thousand users
-user_data = []
+    # Insert data in a single batch using the executemany() method
+    cursor.executemany("""
+        INSERT INTO users (name, age, email, city, is_employee)
+        VALUES (?, ?, ?, ?, ?);""", user_data
+    )
 
-for _ in range(5000):  # Insert 5000 random users
-    name = generate_random_name()
-    age = random.randint(15, 65)
-    email = generate_random_email(name)
-    city = generate_random_city()
-    is_employee = random.choice([True, False])
+def main():
+    # Connect to the database (or create one if it doesn't exist)
+    connection = sqlite3.connect('test_database.db')
+    # Create a cursor object
+    cursor = connection.cursor()
 
-    user_data.append((name, age, email, city, is_employee))
+    create_table(cursor)
 
-# Insert data in a single batch using the executemany() method
-cursor.executemany("""
-    INSERT INTO users (name, age, email, city, is_employee)
-    VALUES (?, ?, ?, ?, ?);""", user_data
-)
+    # Start a transaction (wrapping the inserts within a transaction reduces the time it takes to write to the database)
+    connection.execute("BEGIN TRANSACTION")
 
-# Commit (save) changes
-connection.commit()
+    # Insert 5000 random users
+    insert_random_users(cursor, 5000)
 
-# Retrieve data
-cursor.execute("SELECT * FROM users;")
-rows = cursor.fetchall()
+    # Commit the transaction
+    connection.commit()
 
-# Display retrieved data
-print("\nRandomly generated users:\n")
-for row in rows:
-    print(row)
-print()
+    # Retrieve and display the first 10 rows
+    cursor.execute("SELECT * FROM users LIMIT 10;")
+    rows = cursor.fetchall()
 
-# Close the connection
-connection.close()
+    print("\nFirst 10 rows:\n")
+    for row in rows:
+        print(row)
+    print()
+
+    # Close the connection
+    connection.close()
+
+if __name__ == "__main__":
+    main()
